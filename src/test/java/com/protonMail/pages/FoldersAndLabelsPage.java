7ce3a617f5e195ea94bb8e60799182a49dd8e249
Murat Kurt <mkurt1910@gmail.com>
2020-04-05 02:20:43 +0200

    getAlertMessage() method is updated 2

--- src/test/java/com/protonMail/pages/FoldersAndLabelsPage.java
+++ src/test/java/com/protonMail/pages/FoldersAndLabelsPage.java
@@ -26,7 +26,11 @@ public class FoldersAndLabelsPage extends DashboardPage {
     public WebElement mailbox;
 
     public String getAlertMessage(){
-        wait.until(ExpectedConditions.visibilityOf(alert));
+        try {
+            wait.until(ExpectedConditions.visibilityOf(alert));
+        }catch (NoSuchElementException e){
+            e.printStackTrace();
+        }
         return alert.getText();
     }
 
